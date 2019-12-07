ef8d8cd11dde12c47eb879efd362ad8696928a0b
MarvinHatesOceans <github-bots@sabayon.com>
2019-12-07 16:28:31 +0000

    Sabayon snapshot Sat Dec  7 16:28:31 UTC 2019

--- scripts/create_build.sh
+++ scripts/create_build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+
+
 LUET_REPO="${LUET_REPO:-/root/repo}"
 BUILD_LAYER_NAME="${BUILD_LAYER_NAME:-core-desktop-build}"
 BUILD_LAYER_CATEGORY="${BUILD_LAYER_CATEGORY:-layer}"
 BUILD_LAYER_VERSION="${BUILD_LAYER_VERSION:->=0.1}"
+
 gen_build() {
 local f=$1
+
 # use luet expansions
 PACKAGE_NAME="$(/tmp/yq r $f name)"
 PACKAGE_VERSION="$(/tmp/yq r $f version)"
 PACKAGE_CATEGORY="$(/tmp/yq r $f category)"
+
 basedir="$(dirname $f)"
 cat <<EOF > $basedir/build.yaml
 steps:
@@ -23,13 +28,18 @@ requires:
 EOF
 echo "Generated build definition for $PACKAGE_NAME-$PACKAGE_VERSION ($PACKAGE_CATEGORY)"
 }
+
 clean() {
   rm -rf /tmp/yq
 }
+
 echo "Running builder creator script"
+
 if [ ! -e "/tmp/yq" ]; then
 wget 'https://github.com/mikefarah/yq/releases/download/2.4.1/yq_linux_amd64' -O /tmp/yq
 chmod +x /tmp/yq
 trap clean EXIT
 fi
+
 for f in $(find ${LUET_REPO} -name '*.yaml'); do gen_build $f; done
+
